ea11fef2f92e50994a76f3dbf6dc5a983953450d
Гондурова Дарья <daryavgrei@gmail.com>
2022-10-05 18:45:50 +0300

    add core module

---
+++ core/src/main/java/liga/medical/commonmodule/core/CommonApplication.java
@@ -0,0 +1,12 @@
+package liga.medical.commonmodule.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@ComponentScan
+public class CommonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CommonApplication.class, args);
+    }
+}
---
+++ core/src/test/java/liga/medical/commonmodule/core/CommonApplicationTest.java
@@ -0,0 +1,11 @@
+package liga.medical.commonmodule.core;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CommonApplicationTest {
+    @Test
+    public void contextLoads() {
+    }
+}
